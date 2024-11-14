6c5dfe42b91c4987b459342585138904bab49e7d
mikelo <michele.orlandi@gmail.com>
2024-11-14 15:44:48 +0100

    build in Containerfile and ignoring package-lock.json

--- builds-applications/apps/vertx-site/oc-new-app.sh
+++ builds-applications/apps/vertx-site/oc-new-app.sh
@@ -1,7 +1,17 @@
 #!/bin/env bash
 
-oc new-app --name vertx-site \
-  --build-env MAVEN_MIRROR_URL=http://nexus-common.apps.na410.prod.nextcle.com/repository/java \
-  -i redhat-openjdk18-openshift:1.8 \
-  --context-dir builds-applications/apps/vertx-site \
-  https://github.com/mikelo/DO288-apps#pipeline
+# oc new-app --name vertx-site \
+#   --build-env MAVEN_MIRROR_URL=http://nexus-common.apps.na410.prod.nextcle.com/repository/java \
+#   -i redhat-openjdk18-openshift:1.8 \
+#   --context-dir builds-applications/apps/vertx-site \
+#   https://github.com/mikelo/DO288-apps#pipeline
+
+cd  "$(workspaces.manifest-dir.path)/$(params.MVN_APP_PATH)" || exit 1
+oc new-build --name=$(params.DEPLOY_APP_NAME) \
+  -l app=$(params.DEPLOY_APP_NAME) --binary=true \
+  --image-stream=openshift/java:8 || echo "BC already exists"
+oc start-build $(params.DEPLOY_APP_NAME) --wait=true \
+    --from-file=$(params.DEPLOY_ARTIFACT_NAME)
+oc new-app $(params.DEPLOY_APP_NAME):latest \
+    --name $(params.DEPLOY_APP_NAME) || echo "application exists"
+oc expose svc $(params.DEPLOY_APP_NAME) || echo "route exists"
