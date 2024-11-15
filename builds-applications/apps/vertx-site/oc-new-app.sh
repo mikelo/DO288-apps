75f7fd655e44f8a04d56757af2dcbc52e709234e
mikelo <michele.orlandi@gmail.com>
2024-11-15 12:14:25 +0100

    following the directions from redhat

--- builds-applications/apps/vertx-site/oc-new-app.sh
+++ builds-applications/apps/vertx-site/oc-new-app.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # oc new-app --name vertx-site \
-#   --build-env MAVEN_MIRROR_URL=http://nexus-common.apps.na410.prod.nextcle.com/repository/java \
+#   --build-env MAVEN_MIRROR_URL=http://nexus-common.apps.eu410.prod.nextcle.com/repository/java \
 #   -i redhat-openjdk18-openshift:1.8 \
 #   --context-dir builds-applications/apps/vertx-site \
 #   https://github.com/mikelo/DO288-apps#pipeline
